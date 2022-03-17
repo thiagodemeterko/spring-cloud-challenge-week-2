4897d0d340a318d118c11f16f3ec797c8f3bee07
Thiago Airton Demeterko <thiago.demeterko@gmail.com>
2022-03-17 00:57:38 -0300

    Bulkheads usando Hystrix e Feign

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PagamentoService.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PagamentoService.java
@@ -5,15 +5,28 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
-public class PedidoService {
+public class PagamentoService {
 
     private PedidoClienteComFeign pedidoCliente;
     private PagamentoRepository pagamentoRepo;
 
-    @HystrixCommand(fallbackMethod = "notificaServicoDePedidoParaMudarStatusFallback")
-    public Pagamento notificaServicoDePedidoParaMudarStatus(Long id, MudancaDeStatusDoPedido body) {
+    @HystrixCommand(threadPoolKey = "listaThreadPool")
+    public List<Pagamento> lista() {
+        return pagamentoRepo.findAll();
+    }
+
+    @HystrixCommand(threadPoolKey = "criaThreadPool")
+    public Pagamento cria(Pagamento pagamento) {
+        pagamento.setStatus(Pagamento.Status.CRIADO);
+        return pagamentoRepo.save(pagamento);bul
+    }
+
+    @HystrixCommand(fallbackMethod = "confirmaFallback", threadPoolKey = "confirmaThreadPool")
+    public Pagamento confirma(Long id, MudancaDeStatusDoPedido body) {
         Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(ResourceNotFoundException::new);
         pedidoCliente.notificaServicoDePedidoParaMudarStatus(pagamento.getPedidoId(), body);
         pagamento.setStatus(Pagamento.Status.CONFIRMADO);
@@ -21,7 +34,7 @@ public class PedidoService {
         return pagamento;
     }
 
-    private Pagamento notificaServicoDePedidoParaMudarStatusFallback(Long id, MudancaDeStatusDoPedido body) {
+    private Pagamento confirmaFallback(Long id, MudancaDeStatusDoPedido body) {
         Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(ResourceNotFoundException::new);
         pagamento.setStatus(Pagamento.Status.PROCESSANDO);
         pagamentoRepo.save(pagamento);
